823659aeb3274e1b3440f8f9da6d5732d62a13bb
chris.foster <chris.foster@techdept.co.uk>
2012-11-23 17:43:54 +0000

    Tested latest bundling and minification - still not working with vpp

---
+++ Cms4.TestProject/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace Cms4.TestProject.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/SiteStyles")
+                .Include("~/Content/site.min.css")
+                .Include("~/Content/login.min.css"));
+        }
+    }
+}
--- Cms4.TestProject/Global.asax.cs
+++ Cms4.TestProject/Global.asax.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using Cms4.TestProject.App_Start;
 
 namespace Cms4.TestProject
 {
@@ -19,6 +21,7 @@ namespace Cms4.TestProject
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
